a4a0c6862fd2d16793be45f3285e2bd95dbfd58f
quintanillajeovani@gmail.com <quint@137.125.150.78>
2021-09-09 14:25:53 -0400

    Determining if number is a prime or not

--- CSC211_Week2_Demo/src/main/java/com/mycompany/csc211_week2_demo/NewClass.java
+++ CSC211_Week2_Demo/src/main/java/com/mycompany/csc211_week2_demo/NewClass.java
@@ -1,12 +1,14 @@
-
 package com.mycompany.csc211_week2_demo;
-
-
-public class NewClass {
-    
-    
+public class NewClass {   
     public static void main(String[] args) {
         
+        
+        int arr[][]= {{5,4,10},{-2,11}};
+        
+        for (int row =0; row<2; row++){
+            for(int col=0; col<2 ; col++){
+                System.out.println(arr[row][col] + "  " );
+            }
+        } 
     }
-    
 }
